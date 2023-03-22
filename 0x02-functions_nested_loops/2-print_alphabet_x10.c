38c536375efb80580c50a0ec399e1fea96e7f163
Ayomide1956 <johnltnj1@gmail.com>
2023-03-21 20:05:08 -0700

    fun

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabeth_x10 - print 10 times the alphabeth in lowercase.
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	char chr;
+	int x;
+
+	x = 0;
+
+	while (x < 10)
+	{
+		chr = 'a';
+		while (chr <= 'z')
+		{
+			_putchar(chr);
+			chr++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
