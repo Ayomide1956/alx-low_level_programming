5b011fcddb9c459da311e18985cbac074efcd801
Ayomide1956 <johnltnj1@gmail.com>
2023-03-21 18:13:51 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A program that print put_char.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+Program
+0x02. C - Functions, nested loops
